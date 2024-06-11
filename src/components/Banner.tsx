f1569f236fdb6f91115ee476e2d39183e34db830
John Doe <johndoe@example.com>
2024-06-11 18:01:51 +0300

    fixed mistakes 1.3

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -1,13 +1,16 @@
 import { useState, useEffect } from 'react';
 import { ImSpinner8 } from "react-icons/im";
 import Image from 'next/image';
-import { getDatabase, ref, child, get } from "firebase/database";
+import { v4 as uuidv4 } from 'uuid';
+import { getDatabase, ref, child, get, set } from "firebase/database";
 
 const Banner = () => {
+	const db = getDatabase();
 	const dbRef = ref(getDatabase());
 	const [baner, setBaner] = useState<string>('');
 	const [link, setLink] = useState<string>('');
 	const [showBanner, setShowBanner] = useState<boolean>(false);
+	const [showButton, setShowButton] = useState<boolean>(false);
 	const [close, setClose] = useState<boolean>(true);
 
 	useEffect(() => {
@@ -22,24 +25,38 @@ const Banner = () => {
 	}, []);
 
 	useEffect(() => {
-		if(localStorage.getItem('showBanner') === 'true') return;
-		
-		get(child(dbRef, 'checkbox'))
+		const userId = localStorage.getItem('user') as string;
+		get(child(dbRef, 'user/' + userId))
 		.then((snapshot) => {
 			const data = snapshot.val();
-			if(data.showBanner) {
-				setShowBanner(true);
-				localStorage.setItem('showBanner', 'true');
+			console.log(data);
+			if(data === null){
+				get(child(dbRef, 'checkbox'))
+				.then((snapshot) => {
+					const data = snapshot.val();
+					if(data.showBanner) {
+						const id = uuidv4();
+						if(data.showButton) setShowButton(true);
+						setShowBanner(true);
+						localStorage.setItem('user', id);
+						set(ref(db, 'user/' + id), {
+							deviceId:id,
+							shown_images:{
+								image1:true
+							}
+						});
+					}
+				})
+				.catch((error) => {
+					console.error(error);
+				});
+
+				setTimeout(() => {
+					setClose(false);
+				}, 10000);
 			}
 		})
-		.catch((error) => {
-			console.error(error);
-		});
-
-		setTimeout(() => {
-			setClose(false);
-		}, 10000);
-
+		.catch(err => console.error(err));
 	}, []);
 
 	if(!showBanner) return null;
@@ -47,8 +64,8 @@ const Banner = () => {
 	return (
 		<div className='fixed inset-0 bg-black/80 flex justify-center items-center z-50'>
 			<div className='relative h-[90%] w-[360px] sm:w-[480px] flex flex-col items-center justify-end gap-y-4 sm:gap-y-8 pb-5 sm:pb-10'>
-				<Image src={baner} alt='baner' sizes='100%' className=' object-fill sm:object-contain z-10' fill/>
-				<a className="btn relative z-20" href={link}>перейти</a>
+				<Image src={baner} alt='baner' sizes='100%' className='object-contain z-10' fill/>
+				{showButton && <a className="btn relative z-20" href={link}>перейти</a>}
 				<div className='overflow-hidden relative z-20'>
 					<button className="btn" onClick={() => setShowBanner(false)}>закрыть</button>
 					{close && <div className='absolute inset-0 btn z-10 flex items-center justify-center'>
